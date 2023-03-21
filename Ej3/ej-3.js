b6cf20df1ae3b1bd810dec4b9e9f0312f7b9029f
Exequiel Vega <lfexevega95@gmail.com>
2023-03-21 19:33:10 -0300

    Ejercicio 3

--- Ej3/ej-3.js
+++ Ej3/ej-3.js
@@ -1,1 +1,50 @@
 // 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
+
+class Rectangulo {
+  constructor(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+  }
+
+  modificarPropiedades(alto, ancho) {
+    this.alto = alto;
+    this.ancho = ancho;
+  }
+
+  mostrarPropiedades() {
+    console.log(`Alto: ${this.alto}\nAncho: ${this.ancho}`);
+  }
+
+  calcularPerimetro() {
+    let perimetro = 2 * (this.alto + this.ancho);
+    console.log(`Perímetro: ${perimetro}`);
+  }
+
+  calcularArea() {
+    let area = this.alto * this.ancho;
+    console.log(`Área: ${area}`);
+  }
+}
+
+let rectangulo;
+
+function crearRectangulo() {
+  let alto = parseInt(document.getElementById("alto").value);
+  let ancho = parseInt(document.getElementById("ancho").value);
+  rectangulo = new Rectangulo(alto, ancho);
+  mostrarPropiedades();
+}
+
+function mostrarPropiedades() {
+  rectangulo.mostrarPropiedades();
+  let descripcion = document.getElementById("descripcion");
+  descripcion.textContent = `Alto: ${rectangulo.alto}, Ancho: ${rectangulo.ancho}`;
+}
+
+function calcularPerimetro() {
+  rectangulo.calcularPerimetro();
+}
+
+function calcularArea() {
+  rectangulo.calcularArea();
+}
