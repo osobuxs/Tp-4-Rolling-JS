4ec95dca816e59087c9eeb988655d2dca9d91b45
Exequiel Vega <lfexevega95@gmail.com>
2023-03-21 19:22:08 -0300

    ejercicio 3

---
+++ Ej3/ej-3.js
@@ -0,0 +1,1 @@
+// 3-Escribe una clase que permita crear distintos objetos “rectángulos”, con las propiedades de alto y ancho, mas los métodos necesarios para modificar y mostrar sus propiedades, calcular el perímetro y el área
