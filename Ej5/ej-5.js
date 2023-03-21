76a23a12edc586b1b16b6e031443ec390ec81c25
Exequiel Vega <lfexevega95@gmail.com>
2023-03-21 20:16:07 -0300

    Ejercicio 5

--- Ej5/ej-5.js
+++ Ej5/ej-5.js
@@ -10,47 +10,71 @@
 // Generacion Silent: año de nacimiento desde 1930 a 1948 y su rasgo caracteristico es la Austeridad
 
 class Persona {
-  constructor(nombre, edad, dni, sexo, peso, altura, anioNacimiento) {
+  constructor(nombre, edad, sexo, peso, altura, anioNacimiento) {
     this.nombre = nombre;
     this.edad = edad;
-    this.dni = dni;
     this.sexo = sexo;
     this.peso = peso;
     this.altura = altura;
     this.anioNacimiento = anioNacimiento;
+    this.DNI = this.generaDNI();
   }
 
   mostrarGeneracion() {
-    let generacionDiv = document.getElementById("generacion");
-    let generacion = "";
-    let rasgoCaracteristico = "";
+    let generacion;
+    let rasgoCaracteristico;
 
     if (this.anioNacimiento >= 1994 && this.anioNacimiento <= 2010) {
       generacion = "Generación Z";
-      rasgoCaracteristico = "Irreverencia";
+      rasgoCaracteristico = "irreverencia";
     } else if (this.anioNacimiento >= 1981 && this.anioNacimiento <= 1993) {
       generacion = "Generación Y";
-      rasgoCaracteristico = "Frustración";
+      rasgoCaracteristico = "frustración";
     } else if (this.anioNacimiento >= 1969 && this.anioNacimiento <= 1980) {
       generacion = "Generación X";
-      rasgoCaracteristico = "Obsesión por el éxito";
+      rasgoCaracteristico = "obsesión por el éxito";
     } else if (this.anioNacimiento >= 1949 && this.anioNacimiento <= 1968) {
       generacion = "Generación Baby Boom";
-      rasgoCaracteristico = "Ambición";
+      rasgoCaracteristico = "ambición";
     } else if (this.anioNacimiento >= 1930 && this.anioNacimiento <= 1948) {
       generacion = "Generación Silent";
-      rasgoCaracteristico = "Austeridad";
-    } else {
-      generacion = "No se pudo determinar la generación";
+      rasgoCaracteristico = "austeridad";
     }
 
-    generacionDiv.textContent = `${this.nombre} pertenece a la ${generacion}, cuyo rasgo característico es la ${rasgoCaracteristico}.`;
+    console.log(
+      `La persona de nombre ${this.nombre} pertenece a la ${generacion} y su rasgo característico es la ${rasgoCaracteristico}.`
+    );
+  }
+
+  esMayorDeEdad() {
+    if (this.edad >= 18) {
+      console.log(`${this.nombre} es mayor de edad.`);
+    } else {
+      console.log(`${this.nombre} no es mayor de edad.`);
+    }
+  }
+
+  mostrarDatos() {
+    console.log(`Nombre: ${this.nombre}`);
+    console.log(`Edad: ${this.edad}`);
+    console.log(`Sexo: ${this.sexo}`);
+    console.log(`Peso: ${this.peso}`);
+    console.log(`Altura: ${this.altura}`);
+    console.log(`Año de nacimiento: ${this.anioNacimiento}`);
+    console.log(`DNI: ${this.DNI}`);
+  }
+
+  generaDNI() {
+    return Math.floor(Math.random() * (99999999 - 10000000 + 1) + 10000000);
   }
 }
 
-let persona = new Persona("Juan", 35, "12345678A", "H", 75, 1.8, 1985);
-let persona2 = new Persona("Evelin", 25, "12345678A", "H", 55, 1.6, 1998);
+const persona1 = new Persona("Juan", 35, "H", 70, 1.75, 1988);
+persona1.mostrarGeneracion();
+persona1.esMayorDeEdad();
+persona1.mostrarDatos();
 
-function mostrarGeneracion() {
-  persona.mostrarGeneracion();
-}
+const persona2 = new Persona("Ana", 15, "M", 50, 1.6, 2008);
+persona2.mostrarGeneracion();
+persona2.esMayorDeEdad();
+persona2.mostrarDatos();
